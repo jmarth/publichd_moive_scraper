33e792ce10fc73359424aa65dd1ff811281ff121
Joseph Marth <jm@imga.biz>
2014-12-15 00:33:12 -0600

    updated scraper

--- publichd.py
+++ publichd.py
@@ -4,15 +4,9 @@ from koditorrent import mechanize
 import urllib2
 import cookielib
 import re
-from koditorrent import plugin
-from koditorrent.scrapers import scraper
-from koditorrent.ga import tracked
-from koditorrent.caching import cached_route
-from koditorrent.utils import ensure_fanart
-from koditorrent.library import library_context
 
-username = "nom3kop"
-password = "proyo1"
+username = "username"
+password = "password"
 base_url = "https://publichd.to"
 
 br = mechanize.Browser()
@@ -36,35 +30,7 @@ br.form['username'] = "%s" % username
 br.form['password'] = "%s" % password
 
 br.submit()
-
-@scraper("%s"%plugin.get_setting("publichd_label"), "%s"%plugin.get_setting("publichd_picture"))
-@plugin.route("/publichd")
-@ensure_fanart
-@tracked
-
-
-def publichd_index():
-    yield{
-            "label": "Movies",
-            "path": plugin.url_for("get_movie_name", pageNumber=0),
-            "is_playable": False,
-          }
-
-def get_movie_name(pageNumber):  
-    soup = bs4.BeautifulSoup(br.open("%s/movies?/page=%s" % (base_url, pageNumber)).read())
-    titles =[a.attrs.get('title') for a in soup.select('div.caption a[title]')]
-    names = soup.findAll('div',{'class': 'caption'})
-    new_titles =[]
-    for title in titles:
-        new_titles.append(title.rsplit(' ', 1)[0])
-    for title in new_titles:
-        yield{
-              "label": "%s" % title,
-              "path": "this is fake",
-              "is_playable": False,
-              }
-    #return new_titles
-'''
+    
 def get_movie_page_urls(pageNumber):
     soup = bs4.BeautifulSoup(br.open("%s/movies?/page=%s" % (base_url, pageNumber)).read())
     links = [a.attrs.get('href') for a in soup.select('div.caption a[href^=https://publichd]') ]
@@ -105,24 +71,4 @@ def get_torrent_uri(link):
     print links
     return links
     
-    
-print(get_movie_page_urls(0))
-print
-print
-print
-print
-print(get_movie_name(0))
-links = get_movie_page_urls(0)
-print
-print
-print links[0]
-print(get_torrents_for_movie(links[0]))
-print
-print
-print
-print get_torrents_for_movie_name(links[0])
-
-links1 = get_torrents_for_movie(links[0])
-print get_torrent_uri(links1[0])
-'''
 
